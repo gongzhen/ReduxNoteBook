065d8b0b63b03e8ab264ee8563ad57efc92187c4
gongzhen <gongzhengz@gmail.com>
2017-07-26 23:49:17 -0700

    v1.1

--- src/index.js
+++ src/index.js
@@ -8,36 +8,68 @@ const initialState = {
 
 window.state = initialState;
 
-const onAddNote = () => {
-	const id = window.state.nextNoteId;
-	window.state.notes[id] = {
-		id, 
-		content:''
-	};
-	window.state.nextNoteId++;
-	renderApp();
-}
+const CREATE_NOTE = "CREATE_NOTE";
+const UPDATE_NOTE = "UPDATE_NOTE";
 
-const NoteApp = ({notes}) => (
-	// div
-	<div>
-		// ul
-		<ul className = "note-list">
-		{
-			Object.keys(notes).map(id => (
-				<li className = "note-list-item" key={id}>{id}</li>
-			))
+
+
+const reducer = (state = initialState, action) => {
+	switch(action.type) {
+		case CREATE_NOTE: {
+			const id = state.nextNoteId;
+			const newNote = {
+				id,
+				content:""
+			};
+			return {
+				...state,
+				nextNoteId: id + 1,
+				notes:{
+					...state.notes,
+					[id]: newNote
+				}
+			};
 		}
-		</ul>
-		<button className ="editor-button" onClick={onAddNote}>New Note</button>
-	</div>
+		case UPDATE_NOTE: {
+			const {id, content} = action;
+			const editedNote = {
+				...state.notes[id],
+				content
+			};
+			return {
+				...state,
+				notes:{
+					...state.notes,
+					[id]:editedNote					
+				}
+			};
+		}
+		default:
+			return state;
+	}
+};
+
+
+const handlers = {
+	[CREATE_NOTE]:(state, action) => {
+		return;
+	},
+	[UPDATE_NOTE]:(state, action) => {
+		return;
+	}
+};
+
+const state0 = reducer(undefined, {
+	type: CREATE_NOTE
+});
+
+const state1 = reducer(state0, {
+	type:UPDATE_NOTE,
+	id:1,
+	content:'Hello World'
+});
+
+ReactDOM.render(
+	<pre>{JSON.stringify(state0, null, 2)}{JSON.stringify(state1, null, 2)}</pre>,
+	document.getElementById('root')
 );
-
-const renderApp = () => {
-	ReactDOM.render(
-		<NoteApp notes={window.state.notes} />,
-		document.getElementById('root')
-	);
-}
-
-renderApp();
