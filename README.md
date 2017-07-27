060d9cf53cd62d0d72a47141d84117f47361d5b0
Admin <admin@Admins-iMac.local>
2017-07-27 13:41:01 -0700

    Wire Things up, the Manual Way

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 https://zapier.com/engineering/how-to-build-redux/
+https://jsfiddle.net/justindeal/srnf5n20/3/?utm_source=website&utm_medium=embed&utm_campaign=srnf5n20
 webpack command
 ./node_modules/.bin/webpack -w
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,135 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 ///////////////////////////////
+///////  Components ///////////
+///////////////////////////////
+
+const NoteEditor = ({note, onChangeNote, onCloseNote}) => (
+	<div>
+		<div>
+			<textarea 
+			className = "editor-content" 
+			autoFocus 
+			value={note.content} 
+			onChange = {event => onChangeNote(note.id, event.target.value)}
+			/>
+		</div>
+		<button className="editor-button" onClick={onCloseNote}>
+			Close
+		</button>
+	</div>
+);
+
+const NoteTitle = ({note}) => {
+	const title = note.content.split('\n')[0].replace(/^\s+|\s+$/g, '');
+	if(title === '') {
+		return <i>Untitled</i>;
+	}
+	return <span>{title}></span>;
+};
+
+const NoteLink = ({note, onOpenNote}) => (
+	<li className = "note-list-item">
+		<a href='#' onClick = {() => onOpenNote(note.id)}>
+			<NoteTitle note={note}/>
+		</a>
+	</li>
+);
+
+const NoteList = ({notes, onOpenNote}) => (
+	<ul className = "note-list"> 
+	{
+		Object.keys(notes).map(id => 
+			<NoteLink 
+				key={id} 
+				note={notes[id]} 
+				onOpenNote={onOpenNote} 
+			/>
+		)
+	}
+	</ul>
+);
+
+const NoteApp = ({
+	notes, openNoteId, onAddNote, onChangeNote, onOpenNote, onCloseNote
+}) => (
+	<div>
+	{
+		openNoteId ? 
+		<NoteEditor
+			note = {notes[openNoteId]} 
+			onChangeNote = {onChangeNote} 
+			onCloseNote={onCloseNote} 
+		/> : 
+		<div>
+			<NoteList notes = {notes} onOpenNote = {onOpenNote} />
+			{
+				<button className = "editor-button" onClick={onAddNote} >New Note </button>					
+			}
+		</div>
+	}
+	</div>
+);
+
+class NoteAppContainer extends React.Component {
+	constructor(props) {
+		super();
+		this.state = props.store.getState();
+		this.onAddNote = this.onAddNote.bind(this);
+		this.onChangeNote = this.onChangeNote.bind(this);
+		this.onOpenNote = this.onOpenNote.bind(this);
+		this.onCloseNote = this.onCloseNote.bind(this);
+	}
+
+	componentWillMount() {
+		this.unsubscribe = this.props.store.subscribe(() => this.setState(this.props.store.getState()));		
+	}
+
+	componentWillUnmount() {	
+		this.unsubscribe();
+	}
+
+	onAddNote() {
+		this.props.store.dispatch({
+			type:CREATE_NOTE
+		});
+	}
+
+	onChangeNote(id, content) {
+		this.props.store.dispatch({
+			type:UPDATE_NOTE,
+			id,
+			content
+		});
+	}
+
+	onOpenNote(id) {
+		this.props.store.dispatch({
+			type:OPEN_NOTE,
+			id
+		});
+	}
+
+	onCloseNote() {
+		this.props.store.dispatch({
+			type: CLOSE_NOTE
+		});
+	}
+
+	render(){
+		return(
+			<NoteApp 
+				{...this.state}
+				onAddNote = {this.onAddNote}
+				onChangeNote = {this.onChangeNote}
+				onOpenNote = {this.onOpenNote}
+				onCloseNote = {this.onCloseNote}
+			/>
+		);
+	}
+}
+
+///////////////////////////////
 // Mini Redux implementation //
 ///////////////////////////////
 
@@ -24,7 +153,7 @@ const createStore = reducer => {
 			state = reducer(state, action);
 			subscribers.forEach(handler => handler());
 		},
-		getState:() => state,
+		getState: () => state,
 		subscribe:handler => {
 			subscribers.push(handler);
 			return () => {
@@ -43,6 +172,8 @@ const createStore = reducer => {
 // Our action types //
 //////////////////////
 
+const OPEN_NOTE = 'OPEN_NOTE';
+const CLOSE_NOTE = 'CLOSE_NOTE';
 const CREATE_NOTE = "CREATE_NOTE";
 const UPDATE_NOTE = "UPDATE_NOTE";
 
@@ -52,7 +183,8 @@ const UPDATE_NOTE = "UPDATE_NOTE";
 
 const initialState = {
 	nextNoteId: 1,
-	notes:{}
+	notes:{},
+	openNoteId: null
 };
 
 const reducer = (state = initialState, action) => {
@@ -66,6 +198,7 @@ const reducer = (state = initialState, action) => {
 			return {
 				...state,
 				nextNoteId: id + 1,
+				openNoteId: id,
 				notes:{
 					...state.notes,
 					[id]: newNote
@@ -86,6 +219,18 @@ const reducer = (state = initialState, action) => {
 				}
 			};
 		}
+		case OPEN_NOTE: {
+			return {
+				...state,
+				openNoteId: action.id
+			};
+		}
+		case CLOSE_NOTE: {
+			return {
+				...state,
+				openNoteId:null
+			};
+		}
 		default:
 			return state;
 	}
@@ -101,22 +246,11 @@ const store = createStore(reducer);
 // Render our app whenever the store changes //
 ///////////////////////////////////////////////
 
-store.subscribe(() => {
-	ReactDOM.render(
-	<pre>{JSON.stringify(store.getState(), null, 2)}</pre>,
+ReactDOM.render(
+	<NoteAppContainer store={store}/>,
 	document.getElementById('root')
-	);
-});
+);
 
-//////////////////////
-// Dispatch actions //
-//////////////////////
-
-store.dispatch({type:CREATE_NOTE});
-
-store.getState();
-
-store.dispatch({type:UPDATE_NOTE});
 
 
 
